7d63ef1f61578a81673bf5574b5d70389e647cd1
Heisaa <>
2023-03-15 10:57:27 +0100

    Updates

--- _app/immutable/chunks/1.b36a65b6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3ed4c096.js";export{t as component};
--- _app/immutable/chunks/2.821c59b3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.4b8f0e65.js";export{t as component};
